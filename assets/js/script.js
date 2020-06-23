124e8c7eea4932e6e07aff4b38551428c93855e7
JAVI <ortiz.javier10@gmail.com>
2020-06-22 21:04:56 -0400

    submit event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,24 @@
-var buttonEl = document.querySelector("#save-task");
+// var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-
-
-
-buttonEl.addEventListener("click", function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-});
-
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
     //buttonEl.addEventListener("click", createTaskHandler);
+   
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "this is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-}
+};
+
+
+formEl.addEventListener("submit", createTaskHandler); 
+    // var listItemEl = document.createElement("li");
+    // listItemEl.className = "task-item";
+    // listItemEl.textContent = "This is a new task.";
+    // tasksToDoEl.appendChild(listItemEl);
+
+
